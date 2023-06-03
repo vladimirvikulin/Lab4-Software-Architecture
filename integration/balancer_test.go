b4176e8fb3c84c035dcd8f616aea66e70831d81d
LLumisS <vasa16671@gmail.com>
2023-06-03 15:52:41 +0300

    integration test

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,35 +1,66 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
 
-const baseAddress = "http://balancer:8090"
+func Test(t *testing.T) { TestingT(t) }
+
+type IntegrationSuite struct{}
+
+var _ = Suite(&IntegrationSuite{})
+
+const (
+	baseAddress = "http://balancer:8090"
+	teamName    = "kentiki"
+)
 
 var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
+type RespBody struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func (s *IntegrationSuite) TestLoadBalancer(c *C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
 	// Перевірка доступності балансувальника
 	if !checkBalancer() {
-		t.Skip("Balancer is not available")
+		c.Skip("Balancer is not available")
 	}
 
-	// Виконання запиту до балансувальника
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+	// Дані не знайдено
+	resp1, _ := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	c.Assert(resp1.StatusCode, Equals, http.StatusNotFound)
+
+	// Дані не знайдено
+	resp2, _ := client.Get(fmt.Sprintf("%s/api/v1/some-data?key=kent", baseAddress))
+	c.Assert(resp2.StatusCode, Equals, http.StatusNotFound)
+
+	// Повинно повернути дані під ключем з назвою команди
+	db, err := client.Get(fmt.Sprintf("%s/api/v1/some-data?key=kentiki", baseAddress))
+	c.Assert(err, IsNil)
+
+	var body RespBody
+	err = json.NewDecoder(db.Body).Decode(&body)
+	c.Assert(err, IsNil)
+
+	c.Assert(body.Key, Equals, teamName)
+	c.Assert(body.Value, Not(Equals), "")
+
+	db.Body.Close()
 }
 
 func checkBalancer() bool {
@@ -43,12 +74,9 @@ func checkBalancer() bool {
 	return resp.StatusCode == http.StatusOK
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// Імітуємо бенчмарк-тестування запиту до балансувальника
-	for i := 0; i < b.N; i++ {
+func (s *IntegrationSuite) BenchmarkLoadBalancer(c *C) {
+	for i := 0; i < c.N; i++ {
 		_, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-		if err != nil {
-			b.Error(err)
-		}
+		c.Assert(err, IsNil)
 	}
 }
