204a72a60f2c2222aeb65eb729c625dccf35cea2
vladimirvikulin <v5o4v7a9@gmail.com>
2023-06-03 19:05:05 +0300

    updated tests using gocheck

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -6,7 +6,15 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
+
+func Test(t *testing.T) { TestingT(t) }
+
+type IntegrationSuite struct{}
+
+var _ = Suite(&IntegrationSuite{})
 
 const baseAddress = "http://balancer:8090"
 
@@ -14,28 +22,30 @@ var client = http.Client{
 	Timeout: 18 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
+func (b *IntegrationSuite) TestBalancer(c *C){
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
 	// Виконання запиту до балансувальника
 	for i := 0; i < 5; i++ {
 		time.Sleep(6 * time.Second)
 		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-		if err != nil {
-		t.Error(err)
-		}
-		t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+		c.Assert(err, IsNil)
+		c.Logf("response from [%s]", resp.Header.Get("lb-from"))
 	}
 }
 
-func BenchmarkBalancer(b *testing.B) {
+func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		c.Skip("Integration test is not enabled")
+	}
+	
 	// Імітуємо бенчмарк-тестування запиту до балансувальника
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < c.N; i++ {
 		_, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 		if err != nil {
-			b.Error(err)
+			c.Error(err)
 		}
 	}
 }
